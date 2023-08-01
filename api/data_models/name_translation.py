48c54debedbf904b29264a25b78133e9c57ae0a7
Jae Ro <jaegro93@gmail.com>
2023-08-01 11:28:45 -0400

    Adding changes for structured /translation api response and placeholder directories

--- api/data_models/name_translation.py
+++ api/data_models/name_translation.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
+from typing import List
 
 class KoreanName(BaseModel):
     romanized_english: str
-    korean_parallel: str | None = None
-    
+    korean_parallel: str
+
+class PhoneticTranslation(BaseModel):
+    input: str
+    output: str
+
+class TranslationAPIResponse(BaseModel):
+    korean_name: KoreanName
+    translation: List[PhoneticTranslation] 
--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
-from data_models.name_translation import KoreanName
+from data_models.name_translation import (
+    KoreanName, 
+    PhoneticTranslation,
+    TranslationAPIResponse
+)
 
 app = FastAPI()
 
@@ -17,22 +21,37 @@ async def root() -> dict:
     }
 
 @app.post("/translation/")
-async def translate(name:KoreanName) -> dict:
+async def translate(name:KoreanName) -> TranslationAPIResponse:
     """Function to handle /translation POST request endpoint
 
     Args:
         name (KoreanName): name with english romanization and korean parallel
 
     Returns:
-        dict: JSON response payload as dictionary
+        TranslationAPIResponse: JSON response payload as dictionary
     """
-    print(f"/translation/ called with POST Request Body:\n{' '*4}{name}")
     ## TODO: Add code below to take in name and return a pronounciation string
+    
+    # temporary placeholder for actual translation/phonetic spelling process
+    temp_kor = ["가","나","다","라","마","바","사","아","자","차","카","타","파","하"]
+    temp_eng = ["Gah","Nah","Dah","Rah","Mah","Bah","Sah","Ah","Jah","Chah","Kah","Tah","Pah","Hah"]
 
-
-    return {
-        "message": f"POST Request for { name.romanized_english } received"
-    }
+    phonetic_li = []
+    for i in range(len(name.korean_parallel)):
+        phonetic_li.append(
+            PhoneticTranslation(
+                input=name.korean_parallel[i],
+                output=temp_eng[i]
+            )
+        )
+    
+    # create and return a translation response
+    resp = TranslationAPIResponse(
+        korean_name=name,
+        translation=phonetic_li
+    )
+    print(resp)
+    return resp
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
